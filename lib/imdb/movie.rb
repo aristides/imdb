af54a2dfac2d32b2c1af140bd5c94b6aa44eb7b0
aristides <aristides@aristides-neto.(none)>
2011-03-13 13:46:25 -0300

    Mudando o link para o imdb

--- lib/imdb/movie.rb
+++ lib/imdb/movie.rb
@@ -14,7 +14,7 @@ module Imdb
     #
     def initialize(imdb_id, title, thumblink)
       @id = imdb_id
-      @url = "http://www.imdb.com/title/tt#{imdb_id}/combined"
+      @url = "http://www.imdb.pt/title/tt#{imdb_id}/combined"
       @title = title.gsub(/"/, "") if title
       @thumblink = thumblink
     end
@@ -107,7 +107,7 @@ module Imdb
     
     # Use HTTParty to fetch the raw HTML for this movie.
     def self.find_by_id(imdb_id)
-      open("http://www.imdb.com/title/tt#{imdb_id}/combined")
+      open("http://www.imdb.pt/title/tt#{imdb_id}/combined")
     end
     
     # Convenience method for search
