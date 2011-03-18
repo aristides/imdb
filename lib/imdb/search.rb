f17be7351c9f6e8f0ffa9cefe52054b103002be0
aristides <aristides@aristides-neto.(none)>
2011-03-17 21:46:24 -0300

    Adicionando suporte ao tipo

--- lib/imdb/search.rb
+++ lib/imdb/search.rb
@@ -42,9 +42,7 @@ module Imdb
 
       thumblink = document.at("a[@name='poster'] img")['src']
 
-      if element.parent.innerHTML =~ /<small>(.*)<\/small>/
-        type = $1
-      elsif element.parent.innerHTML =~ /(\(TV\))>/
+      if element.parent.innerHTML =~ /<small>(.*)<\/small>/ || element.parent.innerHTML =~ /(\(TV\))>/
         type = $1
       end
 
